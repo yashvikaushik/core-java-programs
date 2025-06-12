3a2ff47e2ebed7bcb9f3e264b0fcd68b49f97b55
yashvikaushik <yashvikaushik2525@gmail.com>
2025-06-13 00:02:23 +0530

    java beans practice

---
+++ oops/src/beans/Student.java
@@ -0,0 +1,56 @@
+package beans;
+
+public class Student {
+    private String name;
+    private int age;
+    private int height;
+    private int id;
+
+    Student(){
+
+    }
+
+    Student(String name,int age,int height,int id){
+        this.name=name;
+        this.age=age;
+        this.height=height;
+        this.id=id;
+
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int age){
+        this.age=age;
+    }
+
+    public int getID(){
+        return id;
+    }
+
+    public void setID(int id){
+        this.id=id;
+    }
+
+    public int getHeight(){
+        return height;
+    }
+    
+    public void setHeight(int height){
+        this.height=height;
+    }
+
+    public String toString(){
+        return "name: "+name+" age: "+age+" id: "+id+" height: "+height;
+    }
+}
---
+++ oops/src/beans/StudentDemo.java
@@ -0,0 +1,11 @@
+package beans;
+
+public class StudentDemo {
+    public static void main(String[] args) {
+        Student student1=new Student("priya",5,6,25);
+         student1.setAge(10);
+         System.out.println(student1);
+         
+    }
+    
+}
